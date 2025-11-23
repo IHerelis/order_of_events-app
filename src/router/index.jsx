67c8a1dca11d6043b7305459c2e5cefa67b06690
IHerelis <agiresubito@gmail.com>
2025-11-23 13:54:39 +0200

    added basename in router and packege.json

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,16 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import HomePage from "../components/pages/home-page/HomePage";
 
-export const router = createBrowserRouter ([
+export const router = createBrowserRouter (
   {
-    path: '/',
-    element: <App />,
-    errorElement: <h1>Error Page</h1>,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-    ]
-  }
-]);
+    basename: '/order_of_events-app'
+  },
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <h1>Error Page</h1>,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+      ]
+    }
+  ]);
