1f4d7665d724b4fb9746601284edec248a1c98be
IHerelis <agiresubito@gmail.com>
2026-01-11 23:02:44 +0200

    changed adapterLocale to uk-UA

--- src/components/OtherComponents/CalendarBlock/CalendarBlock.jsx
+++ src/components/OtherComponents/CalendarBlock/CalendarBlock.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import dayjs from 'dayjs';
-import 'dayjs/locale/de';
+import 'dayjs/locale/uk';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
@@ -11,7 +11,7 @@ const CalendarBlock = () => {
   const [value, setValue] = React.useState(dayjs(new Date));
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="uk">
       <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
     </LocalizationProvider>
 
