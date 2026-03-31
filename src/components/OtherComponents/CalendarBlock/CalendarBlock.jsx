e8b503b96401b0b9acc2d0f024c523fe96b8bf91
IHerelis <agiresubito@gmail.com>
2026-03-31 20:50:58 +0300

    adapted CalendarBlock to the container size

--- src/components/OtherComponents/CalendarBlock/CalendarBlock.jsx
+++ src/components/OtherComponents/CalendarBlock/CalendarBlock.jsx
@@ -8,11 +8,58 @@ import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 
 const CalendarBlock = () => {
-  const [value, setValue] = React.useState(dayjs(new Date));
+  const [value, setValue] = React.useState(dayjs());
+  // const [value, setValue] = React.useState(dayjs(new Date));
 
   return (
+    // <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="uk">
+    //   <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+    // </LocalizationProvider>
+
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="uk">
-      <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+      <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} 
+        sx={{
+          height: '242px',
+          width: '270px',
+          pt: '2px',
+
+          '& .MuiPickersCalendarHeader-root': {
+            mt: '2px',
+            mb: '2px',
+            minHeight: '32px',
+            maxHeight: '32px',
+          },
+          '& .MuiPickersCalendarHeader-root .MuiButtonBase-root': {
+            p: '4px',
+          },
+
+          '& .MuiDayCalendar-header': {
+            pl: '24px',
+            pr: '22px',
+          },
+          '& .MuiDayCalendar-header .MuiDayCalendar-weekDayLabel': {
+            height: '16px',
+          },
+
+          '& .MuiPickersSlideTransition-root': {
+            minHeight: '182px',
+          },
+
+          '& .MuiDayCalendar-weekContainer .MuiPickersDay-root': {
+            width: '28px',
+            height: '28px',
+          },
+
+          '& .MuiDayCalendar-weekContainer .MuiPickersDay-root.Mui-selected': {
+            backgroundColor: 'rgb(40, 141, 40)',
+            fontSize: '14px',
+          },
+
+          '& .MuiYearCalendar-root': {
+            width: '270px',
+          },
+        }}
+      />
     </LocalizationProvider>
 
     // <LocalizationProvider dateAdapter={AdapterDayjs}>
